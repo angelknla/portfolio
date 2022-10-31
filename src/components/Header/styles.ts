5d53c7ecc3402ec5d8eb5e02c93efa7f2274b6e1
Angel Canela <angel.canela@kurtgeiger.com>
2022-10-31 23:04:14 +0000

    ease in out

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -192,7 +192,6 @@ export const Container = styled.header<props>`
       background-color: #fff;
       box-shadow: 1px 1px 20px 0px rgba(0,0,0,0.2);
       border-radius: 0px 0px 15px 15px;
-      z-index: 5;
 
       a.button {
         background-color: #016fb9;
@@ -241,19 +240,19 @@ export const Container = styled.header<props>`
       &.active {
         opacity: 1;
         visibility: visible;
-        animation: expandDown 0.65s linear forwards;
-        -webkit-animation: expandDown 0.65s linear forwards;
-        -moz-animation: expandDown 0.65s linear forwards;
-        -o-animation: expandDown 0.65s linear forwards;
+        animation: expandDown 0.65s ease-in-out forwards;
+        -webkit-animation: expandDown 0.65s ease-in-out forwards;
+        -moz-animation: expandDown 0.65s ease-in-out forwards;
+        -o-animation: expandDown 0.65s ease-in-out forwards;
       }
 
        &.inactive {
         opacity: 1;
         visibility: visible;
-        animation: expandUp 0.75s linear forwards;
-        -webkit-animation: expandUp 0.75s linear forwards;
-        -moz-animation: expandUp 0.75s linear forwards;
-        -o-animation: expandUp 0.75s linear forwards;
+        animation: expandUp 0.75s ease-in-out forwards;
+        -webkit-animation: expandUp 0.75s ease-in-out forwards;
+        -moz-animation: expandUp 0.75s ease-in-out forwards;
+        -o-animation: expandUp 0.75s ease-in-out forwards;
       }
     }
   }
