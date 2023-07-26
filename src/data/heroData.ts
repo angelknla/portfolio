81d25959122f044f42bb90a8a06c9294ea1081c2
Angel Canela Gallardo <gbAnCaGa@a-jj6tz33.corp.lego.com>
2023-07-26 10:51:08 +0100

    chore: changed description

--- src/data/heroData.ts
+++ src/data/heroData.ts
@@ -2,22 +2,22 @@ export const heroData = {
     english: {
       greeting: 'Hello, this is',
       name: "Angel Canela",
-      job: 'Front-end Software Engineer',
-      intro: "I am a Junior Software Engineer with experience developing web-based systems.",
+      job: 'Full-stack Software Engineer',
+      intro: "I am a high-level Software Engineer with experience designing, testing and implementing software applications.",
       contact: "Contact",
     }, 
     spanish: {
       greeting: 'Hola, me llamo',
       name: "Angel Canela",
       job: 'Ingeniero de Software',
-      intro: "Soy Ingeniero de Software Junior con experiencia en desarrollo de sistemas web.",
+      intro: "Soy Ingeniero de Software con experiencia en el desarrollo de sistemas web.",
       contact: "Contacto"
     },
     japanese: {
       greeting: 'こんにちは',
       name: "Angel Canela",
       job: 'ソフトウェアエンジニア',
-      intro: "私は、Web ベースのシステムの開発経験を持つジュニア ソフトウェア エンジニアです.",
+      intro: "私は、Web ベースのシステムの開発経験を持つソフトウェア エンジニアです.",
       contact: "連絡"
     }
 };
