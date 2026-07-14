bfa8ba5f86e63571a733bf767692c06277e5f9f2
Angel Canela <angel.gallardo@lego.com>
2026-07-14 10:02:21 +0100

    chore: PRTFLIO fix CSS

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -14,45 +14,51 @@ export const Footer = () => {
 
   return (
     <footer className={styles.container}>
-      <a href='/' className={styles.logo}>
-        <span>Angel</span>
-        <span> Canela</span>
-      </a>
-      <div className={styles.message}>
-        <p>{message}</p>
-      </div>
-
-      <div className={styles.socialMedia}>
-        <a
-          href='https://www.linkedin.com/in/angel-canela/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <Image
-            src='/assets/linkedin.webp'
-            alt='Linkedin'
-            width={24}
-            height={24}
-          />
+      <div className={styles.inner}>
+        <a href='/' className={styles.logo}>
+          <span>Angel</span>
+          <span> Canela</span>
         </a>
+        <div className={styles.message}>
+          <p>{message}</p>
+        </div>
 
-        <a href='https://github.com/angelknla' target='_blank' rel='noreferrer'>
-          <Image
-            src='/assets/github.webp'
-            alt='GitHub'
-            width={24}
-            height={24}
-          />
-        </a>
+        <div className={styles.socialMedia}>
+          <a
+            href='https://www.linkedin.com/in/angel-canela/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Image
+              src='/assets/linkedin.webp'
+              alt='Linkedin'
+              width={24}
+              height={24}
+            />
+          </a>
 
-        <a href='b' target='_blank' rel='noreferrer'>
-          <Image
-            src='/assets/instagram.webp'
-            alt='Instagram'
-            width={24}
-            height={24}
-          />
-        </a>
+          <a
+            href='https://github.com/angelknla'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Image
+              src='/assets/github.webp'
+              alt='GitHub'
+              width={24}
+              height={24}
+            />
+          </a>
+
+          <a href='b' target='_blank' rel='noreferrer'>
+            <Image
+              src='/assets/instagram.webp'
+              alt='Instagram'
+              width={24}
+              height={24}
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
